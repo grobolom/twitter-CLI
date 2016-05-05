ecaefeea6b8adac404c67d3e398dc6e82a0fdf60
Vasja Volin <vasja.volin@gmail.com>
2016-05-04 21:19:27 -0400

    add a mockup main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    print 'fetch data from the api'
+    print 'loop'
+    print '   shove data + action into reducer to get a visual representation'
+    print '   push it into a renderer to get an 80x40 printout of it'
+    print '   wait for commands from the user'
+
+if __name__ == "__main__":
+    main()
