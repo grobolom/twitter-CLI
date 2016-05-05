adf930f9e2236668170764a210899dbe585ab8c4
Vasja Volin <vasja.volin@gmail.com>
2016-05-04 21:27:28 -0400

    play around with reading from stdin

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def main():
-    print 'fetch data from the api'
-    print 'loop'
-    print '   shove data + action into reducer to get a visual representation'
-    print '   push it into a renderer to get an 80x40 printout of it'
-    print '   wait for commands from the user'
+    print('fetch data from the api')
+    print('loop')
+    print('   shove data + action into reducer to get a visual representation')
+    print('   push it into a renderer to get an 80x40 printout of it')
+    print('   wait for commands from the user')
+
+    while 1 == 1:
+        x = raw_input()
+        print(x)
 
 if __name__ == "__main__":
     main()
