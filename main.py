7b23f2cc83695f9a9888b038f6bd1bc3967ae49d
Vasja Volin <vasja.volin@gmail.com>
2016-05-07 18:37:22 -0400

    spike out working with the twitter client

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import sys
 import readchar
+import json
+
+from twitter import Twitter, OAuth
 
 def main():
     print('fetch data from the api')
+
+    fetch()
+
     print('loop')
     print('   shove data + action into reducer to get a visual representation')
     print('   push it into a renderer to get an 80x40 printout of it')
@@ -12,6 +18,21 @@ def main():
     while key != 27:
         key = ord(readchar.readchar())
         print(key)
+
+def fetch():
+    with open('config/twitter.json') as twitter_config:
+        config = json.load(twitter_config)
+
+    user = config['user']
+    access_key = config['access_key']
+    access_secret = config['access_secret']
+    consumer_key = config['consumer_key']
+    consumer_secret = config['consumer_secret']
+
+    twitter = Twitter(
+        auth = OAuth(access_key, access_secret, consumer_key, consumer_secret))
+
+    print(twitter.statuses.user_timeline(screen_name=user))
 
 if __name__ == "__main__":
     main()
