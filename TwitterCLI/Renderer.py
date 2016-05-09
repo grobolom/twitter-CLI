5bede20254d9757f4f0fdab9a23511644d61828f
Vasja Volin <vasja.volin@gmail.com>
2016-05-08 23:56:54 -0400

    add ansi terminal colors to the tweet renderer

--- TwitterCLI/Renderer.py
+++ TwitterCLI/Renderer.py
@@ -1,7 +1,15 @@
+class TermAnsiColors:
+    OKGREEN = '\033[92m'
+    ENDC = '\033[0m'
+
 class Renderer:
     def render(self, tweet):
         lines = tweet.text.split("\n")
 
-        first_line = [ tweet.author.rjust(15) + " " + lines[0] ]
+        author_part = TermAnsiColors.OKGREEN + \
+            tweet.author.rjust(15) + \
+            " " + \
+            TermAnsiColors.ENDC
+        first_line = [ author_part + lines[0] ]
         rest = [ " " * 16 + line for line in lines[1::]]
         return first_line + rest
