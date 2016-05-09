2869a067b728392ee1ff4a645b7fe09f2cdf5ed7
Vasja Volin <vasja.volin@gmail.com>
2016-05-09 06:53:14 -0400

    move fetching tweets to its own function

---
+++ TwitterCLI/fetch_tweets.py
@@ -0,0 +1,37 @@
+import json
+from twitter import Twitter, OAuth
+
+def fetch_tweets():
+    timeline = None
+    try:
+        with open('config/data.json') as cached_data:
+            timeline = json.load(cached_data)
+    except IOError:
+        pass
+
+    if not timeline:
+        print('fetching')
+        with open('config/twitter.json') as twitter_config:
+            config = json.load(twitter_config)
+
+        user = config['user']
+        access_key = config['access_key']
+        access_secret = config['access_secret']
+        consumer_key = config['consumer_key']
+        consumer_secret = config['consumer_secret']
+
+        twitter = Twitter(
+            auth = OAuth(
+                access_key,
+                access_secret,
+                consumer_key,
+                consumer_secret
+            )
+        )
+
+        timeline = twitter.statuses.user_timeline(screen_name=user)
+
+        with open('config/data.json', 'w') as temp_data:
+            json.dump(timeline, temp_data)
+
+    return timeline
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import sys
 import readchar
-import json
 import shutil
 import html
 import os
 
-from twitter import Twitter, OAuth
 from TwitterCLI.Tweet import Tweet
 from TwitterCLI.Screen import Screen
+from TwitterCLI.fetch_tweets import fetch_tweets
 
 from blessed import Terminal
 
 def main():
-    timeline = fetch()
+    timeline = fetch_tweets()
     dims = shutil.get_terminal_size()
     screen = Screen(dims[1], dims[0])
 
@@ -39,42 +38,6 @@ def render(term, screen, tweets, do):
         print(term.clear)
     with term.location(0, 0):
         screen.render(tweets)
-
-
-def fetch():
-    timeline = None
-    try:
-        with open('config/data.json') as cached_data:
-            timeline = json.load(cached_data)
-    except IOError:
-        pass
-
-    if not timeline:
-        print('fetching')
-        with open('config/twitter.json') as twitter_config:
-            config = json.load(twitter_config)
-
-        user = config['user']
-        access_key = config['access_key']
-        access_secret = config['access_secret']
-        consumer_key = config['consumer_key']
-        consumer_secret = config['consumer_secret']
-
-        twitter = Twitter(
-            auth = OAuth(
-                access_key,
-                access_secret,
-                consumer_key,
-                consumer_secret
-            )
-        )
-
-        timeline = twitter.statuses.user_timeline(screen_name=user)
-
-        with open('config/data.json', 'w') as temp_data:
-            json.dump(timeline, temp_data)
-
-    return timeline
 
 if __name__ == "__main__":
     main()
