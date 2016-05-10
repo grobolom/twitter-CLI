28ad2c60eb284f23a9edcb204240a27a30c7dee0
Vasja Volin <vasja.volin@gmail.com>
2016-05-10 01:25:17 -0400

    update to using Screen for our main testing file

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import readchar
 import shutil
 import html
 import os
+import re
 
 from TwitterCLI.Tweet import Tweet
+from TwitterCLI.Screen import Screen
 from TwitterCLI.views.TimelineView import TimelineView
 from TwitterCLI.fetch_tweets import fetch_tweets
 
@@ -13,33 +15,32 @@ from blessed import Terminal
 def main():
     timeline = fetch_tweets()
     dims = shutil.get_terminal_size()
-    tl = TimelineView(80, 5)
+    tl = TimelineView(80, dims[1] - 1)
 
     tweets = []
     for tweet in timeline:
         author = tweet['user']['screen_name']
-        text = html.unescape(tweet['text'].replace('\u2026','').rstrip(' \n'))
+        text = re.sub(r'[^\x00-\x7f]', r'.', html.unescape(tweet['text']))
+        text = text.replace('\n', ' ')
         tweets.append(Tweet(author, text))
 
     term = Terminal()
     key = 0
     do = ''
+    cursor = 0
+    s = Screen()
 
     with term.fullscreen():
         while key != '\x03':
-            if key == 'd':
-                tweets = tweets[1::]
-                do = 'clear'
-            render(term, tweets, do, tl)
+            if key == 'k':
+                cursor -= 1
+            if key == 'j':
+                cursor += 1
+            s.render(term, [
+                (0, 0, tl.render(tweets, cursor)),
+                (158, 0, [str(len(tweets)), str(cursor)]),
+            ])
             key = readchar.readkey()
-
-def render(term, tweets, do, timeline):
-    if do == 'clear':
-        print(term.clear)
-    with term.location(0, 0):
-        lines = timeline.render(tweets, 0)
-        for t in lines:
-            print(t)
 
 if __name__ == "__main__":
     main()
