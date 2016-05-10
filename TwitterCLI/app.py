03f4bc2bd15f2d4fb789c3a80685f5c0b129ff83
Vasja Volin <vasja.volin@gmail.com>
2016-05-10 03:31:46 -0400

    extracted main loop to its own class for easier refactoring

---
+++ TwitterCLI/app.py
@@ -0,0 +1,61 @@
+import os
+
+from TwitterCLI.Screen import Screen
+from TwitterCLI.views.TimelineView import TimelineView
+from TwitterCLI.fetch_tweets import fetch_tweets
+from TwitterCLI.TweetBuilder import TweetBuilder
+from TwitterCLI.reducers import RootReducer
+
+from blessed import Terminal
+
+class TwitterClient:
+
+    def __init__(self):
+        self.screen   = Screen()
+        self.terminal = Terminal()
+        self.reducer  = RootReducer()
+        self.state    = self._initialState()
+
+        self.timelineView = TimelineView()
+
+    def run(self, dims):
+        state = self._initialState()
+        with self.terminal.fullscreen():
+            with self.terminal.cbreak():
+                key = ''
+                while key != '\x03':
+                    if key == 'x':
+                        os.system('clear')
+                    action = self._actions(key)
+                    state = self.reducer.reduce(state, action)
+                    self.render(dims, state)
+                    key = self.terminal.inkey(timeout = 5)
+
+    def _initialState(self):
+        return {
+            'cursor': 0,
+            'cursor_max': '200',
+            'tweets': self._getTweets(),
+        }
+
+    def _getTweets(self):
+        return fetch_tweets()
+
+    def _actions(self, key):
+        key_to_action_map = {
+            'k': 'CURSOR_UP',
+            'j': 'CURSOR_DOWN',
+        }
+        if key in key_to_action_map:
+            return key_to_action_map[ key ]
+        return None
+
+    def render(self, dims, state):
+        self.terminal.move(0, 0)
+        self.screen.render(self.terminal, [
+            (0, 0, self.timelineView.render(
+                state['tweets'], state['cursor'], dims[0], dims[1] - 1
+            )),
+            (dims[0] - 19, 0, ['some bullshit'])
+        ])
+        self.terminal.move(0, 0)
--- main.py
+++ main.py
@@ -1,46 +1,10 @@
-import sys
-import readchar
 import shutil
-import html
-import os
-import re
-
-from TwitterCLI.Tweet import Tweet
-from TwitterCLI.Screen import Screen
-from TwitterCLI.views.TimelineView import TimelineView
-from TwitterCLI.fetch_tweets import fetch_tweets
-
-from blessed import Terminal
+from TwitterCLI.app import TwitterClient
 
 def main():
-    timeline = fetch_tweets()
-    dims = shutil.get_terminal_size()
-    tl = TimelineView(80, dims[1] - 1)
-
-    tweets = []
-    for tweet in timeline:
-        author = tweet['user']['screen_name']
-        text = re.sub(r'[^\x00-\x7f]', r'.', html.unescape(tweet['text']))
-        text = text.replace('\n', ' ')
-        tweets.append(Tweet(author, text))
-
-    term = Terminal()
-    key = 0
-    do = ''
-    cursor = 0
-    s = Screen()
-
-    with term.fullscreen():
-        while key != '\x03':
-            if key == 'k':
-                cursor -= 1
-            if key == 'j':
-                cursor += 1
-            s.render(term, [
-                (0, 0, tl.render(tweets, cursor)),
-                (158, 0, [str(len(tweets)), str(cursor)]),
-            ])
-            key = readchar.readkey()
+    app = TwitterClient()
+    app.run(shutil.get_terminal_size())
+    return
 
 if __name__ == "__main__":
     main()
