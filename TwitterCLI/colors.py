9c2f0c4230609286cffddaccd60b463a04de1da5
Vasja Volin <vasja.volin@gmail.com>
2016-05-11 03:17:17 -0400

    add utility packages for working with terminal colors

---
+++ TwitterCLI/colors.py
@@ -0,0 +1,9 @@
+import TwitterCLI.utils.terminal_colors as colors
+
+USER     = colors.GREEN
+LINK     = colors.LIGHT_BLUE
+MENTION  = colors.LIGHT_YELLOW
+HASHTAG  = colors.LIGHT_PURPLE
+
+def color_string(string, color):
+    return color + string + colors.ENDC
---
+++ TwitterCLI/utils/terminal_colors.py
@@ -0,0 +1,6 @@
+GREEN        = '\033[92m'
+LIGHT_BLUE   = '\033[94m'
+LIGHT_YELLOW = '\033[33m'
+LIGHT_PURPLE = '\033[95m'
+
+ENDC = '\033[0m'
