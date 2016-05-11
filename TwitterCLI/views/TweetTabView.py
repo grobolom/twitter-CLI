09e1e3431594106fcf4bfb94e67ff7a07a4e2a02
Vasja Volin <vasja.volin@gmail.com>
2016-05-10 23:03:13 -0400

    add rendering different types of lists in the tab view

--- TwitterCLI/views/TweetTabView.py
+++ TwitterCLI/views/TweetTabView.py
@@ -10,10 +10,24 @@ class TweetTabView():
 
         lines += header
 
+        timelines = []
+        lists = []
+
         if 'available_lists' in state:
             _lists = state['available_lists']
             for l in _lists:
-                line = ' '.join([ e.capitalize() for e in l.split('_')])
-                lines += [ ' ' + line ]
+                if '.' in l:
+                    (a, n) = l.split('.')
+                    name = ' '.join([ e.capitalize() for e in n.split('_')])
+                    lists += [ '    ' + name ]
+                else:
+                    line = ' '.join([ e.capitalize() for e in l.split('_')])
+                    lines += [ '  ' + line ]
+
+        lines += timelines
+        if lists:
+            lines += ['']
+            lines += ['  Lists:']
+            lines += lists
 
         return [ line.ljust(20) for line in lines ]
--- tests/testTweetTabView.py
+++ tests/testTweetTabView.py
@@ -29,6 +29,31 @@ class TestTweetTabView(unittest.TestCase):
             'TwitterCLI          ',
             '@grobolom           ',
             '                    ',
-            ' Tweets             ',
-            ' Home Timeline      ',
+            '  Tweets            ',
+            '  Home Timeline     ',
         ])
+
+    def test_it_should_render_lists(self):
+        v = TweetTabView()
+        state = {
+            'username': 'grobolom',
+            'available_lists': [
+                'tweets',
+                'home_timeline',
+                'list.friends',
+                'list.other_list',
+            ]
+        }
+        actual = v.render(state)
+        self.assertEqual(actual, [
+            'TwitterCLI          ',
+            '@grobolom           ',
+            '                    ',
+            '  Tweets            ',
+            '  Home Timeline     ',
+            '                    ',
+            '  Lists:            ',
+            '    Friends         ',
+            '    Other List      ',
+        ])
+
