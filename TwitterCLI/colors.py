8235c530ed137286d9d9ba316533593ff1a62761
Vasja Volin <vasja.volin@gmail.com>
2016-05-11 03:31:11 -0400

    add helper functons for color formatting

--- TwitterCLI/colors.py
+++ TwitterCLI/colors.py
@@ -7,3 +7,15 @@ HASHTAG  = colors.LIGHT_PURPLE
 
 def color(string, color):
     return color + string + colors.ENDC
+
+def user(string):
+    return color(string, USER)
+
+def link(string):
+    return color(string, LINK)
+
+def mention(string):
+    return color(string, MENTION)
+
+def hashtag(string):
+    return color(string, HASHTAG)
