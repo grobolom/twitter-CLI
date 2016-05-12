d1b2a1458f540fd09101616fda61e1c32686ad9c
Vasja Volin <vasja.volin@gmail.com>
2016-05-12 00:25:35 -0400

    add a tweet source class for pulling more complex tweets later

--- TweetSource/TweetSource.py
+++ TweetSource/TweetSource.py
@@ -1,2 +1,15 @@
 class TweetSource:
-    pass
+    def __init__(self, config, twitter, mongodb):
+        self.config = config
+        self.twitter = twitter
+
+    def _getNewTweets(self, since=None):
+        params = {}
+        if since:
+            params['since'] = since
+
+        return self.twitter.statuses.user_timeline(
+            screen_name = self.config['user'],
+            include_rts = False,
+            **params
+        )
--- tests/testTweetSource.py
+++ tests/testTweetSource.py
@@ -1,7 +1,25 @@
 import unittest
+from mock import MagicMock as Mock
 
 from TweetSource.TweetSource import TweetSource
 
 class TestTweetSource(unittest.TestCase):
-    def it_should_try_fetching_tweets(self):
-        pass
+    def setUp(self):
+        self.twitter = Mock()
+        self.mongodb = Mock()
+        self.config = { 'user' : 'grob' }
+        self.ts = TweetSource(self.config, self.twitter, self.mongodb)
+
+    def test_it_should_fetch_latest_tweets_from_twitter(self):
+        self.ts._getNewTweets(since=800)
+        self.twitter.statuses.user_timeline.assert_called_once_with(
+            screen_name='grob',
+            include_rts=False,
+            since=800
+        )
+    def test_it_should_fetch_some_tweets_from_twitter(self):
+        self.ts._getNewTweets()
+        self.twitter.statuses.user_timeline.assert_called_once_with(
+            screen_name='grob',
+            include_rts=False
+        )
