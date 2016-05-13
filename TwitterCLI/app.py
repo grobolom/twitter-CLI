744423f987854a36879158e2de14b33343f5f9a1
Vasja Volin <vasja.volin@gmail.com>
2016-05-13 02:55:16 -0400

    reorganize app.py for testing

--- TwitterCLI/app.py
+++ TwitterCLI/app.py
@@ -39,36 +39,50 @@ class TwitterClient:
         self.state    = self._initialState()
 
     def run(self):
-        old_state = {}
-        state = self._initialState()
         try:
-            with self.terminal.fullscreen():
-                key = ''
-                while True:
-
-                    # TODO: move this somewhere where it makes sense
-                    dims = shutil.get_terminal_size()
-
-                    state['screen_width']  = dims[0]
-                    state['screen_height'] = dims[1]
-
-                    action = self._actions(key)
-
-                    state = self.reducer.reduce(state, action)
-
-                    if state != old_state:
-                        self.render(state)
-
-                    old_state = dict.copy(state)
-
-                    with self.terminal.cbreak():
-                        key = self.terminal.inkey(timeout = 1)
-
-                    if key.is_sequence:
-                        key = key.name
+            self.terminal.enter_fullscreen()
+            self._startEventLoop()
+            self.terminal.exit_fullscreen()
         except KeyboardInterrupt:
+            self.terminal.exit_fullscreen()
+            print(self.terminal.clear)
             print('seeya!')
-            return
+        return
+
+    def _startEventLoop(self):
+        state     = self._initialState()
+        key       = ''
+
+        self.terminal.enter_fullscreen()
+        state = self._appendDims(state)
+        self.render(state)
+        while True:
+            state = self._appendDims(state)
+            state = self._handleState(key, state)
+            key = self._handleKey()
+
+    def _handleKey(self):
+        key = ''
+        with self.terminal.cbreak():
+            key = self.terminal.inkey(timeout = 1)
+        if key.is_sequence:
+            key = key.name
+        return key
+
+    def _handleState(self, key, state):
+        action    = self._actions(key)
+        new_state = self.reducer.reduce(state, action)
+
+        if state != new_state:
+            self.render(new_state)
+
+        return new_state
+
+    def _appendDims(self, state):
+        dims = shutil.get_terminal_size()
+        state['screen_width']  = dims[0]
+        state['screen_height'] = dims[1]
+        return state
 
     def _initialState(self):
         lists = {
