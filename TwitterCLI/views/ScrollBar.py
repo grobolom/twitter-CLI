3fe57141135fe09e0e0b024b179db54d89356a22
Vasja Volin <vasja.volin@gmail.com>
2016-05-13 15:43:17 -0400

    add test and implementation for Scrollbar View

--- TwitterCLI/views/ScrollBar.py
+++ TwitterCLI/views/ScrollBar.py
@@ -1,2 +1,11 @@
 class ScrollBar:
-    pass
+    def render(height, cursor, c_max, page_height):
+        cursor_size  = int(height * (page_height / c_max))
+        cursor_start = int(height * (cursor / c_max))
+        cursor_end   = cursor_start + cursor_size
+
+        left      = [ " " for e in range(0, cursor_start) ]
+        scrollbar = [ "X" for e in range(0, cursor_size) ]
+        right     = [ " " for e in range(cursor_end, height) ]
+
+        return left + scrollbar + right
--- tests/testScrollBarView.py
+++ tests/testScrollBarView.py
@@ -2,4 +2,33 @@ import unittest
 from TwitterCLI.views import ScrollBar
 
 class TestScrollBarView(unittest.TestCase):
-    pass
+    def test_it_should_display_a_scrollbar(self):
+        height      = 20
+        cursor      = 17
+        cursor_max  = 100
+        page_height = 10
+
+        scrollbar = ScrollBar()
+        result = ScrollBar.render(
+            height,
+            cursor,
+            cursor_max,
+            page_height
+        )
+        self.assertEqual("".join(result),
+            "   XX               ")
+
+        height      = 10
+        cursor      = 35
+        cursor_max  = 100
+        page_height = 10
+
+        scrollbar = ScrollBar()
+        result = ScrollBar.render(
+            height,
+            cursor,
+            cursor_max,
+            page_height
+        )
+        self.assertEqual("".join(result),
+            "   X      ")
