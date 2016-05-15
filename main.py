359057e4611e28b2d168899b33bff15b4a8744fa
Vasja Volin <vasja.volin@gmail.com>
2016-05-15 06:12:47 -0400

    added a sample thread that pushes things to the queue

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import shutil
 from queue import Queue
 from TwitterCLI.app import TwitterClient
 
+from time import sleep
+from threading import Thread
+
+def move_tab_later(q):
+    sleep(10)
+    action = { 'name': 'SWITCH_TAB' }
+    q.put(action)
+
 def main():
     q = Queue()
+    t = Thread(target=move_tab_later, args=(q,))
+    t.daemon = True
+    t.start()
+
     app = TwitterClient(q)
     app.run()
     return
