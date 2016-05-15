8f24df958d08bade37a9eae5c9ceaac83898cb2f
Vasja Volin <vasja.volin@gmail.com>
2016-05-15 08:56:37 -0400

    expand the tweet fetcher to pull all the different types of lists

--- TweetSource/app.py
+++ TweetSource/app.py
@@ -1,8 +1,32 @@
-def main():
+from TweetSource.TweetSource import TweetSource
+from TweetSource.modules import TweetFetcher
+from TweetSource.utils import getTwitter
+
+def main(client_queue):
     with open('config/twitter.json') as twitter_config:
         config = json.load(twitter_config)
     twitter = getTwitter(config)
     tweetSource = TweetSource(config, twitter)
     tweetFetcher = TweetFetcher(tweetSource)
 
-    sleep(1)
+    sleep(2)
+    client_queue.put({
+        'name': 'NEW_TWEETS',
+        'list': 'home_timeline',
+        'tweets': tweetFetcher.getHomeTimeline()
+    })
+    sleep(2)
+    client_queue.put({
+        'name': 'NEW_TWEETS',
+        'list': 'tweets',
+        'tweets': tweetFetcher.getTweets()
+    })
+    sleep(2)
+    lists = tweetFetcher.getLists()
+    for _list in lists:
+        sleep(2)
+        client_queue.put({
+            'name': 'NEW_TWEETS',
+            'list': 'list.' + _list,
+            'tweets': tweetFetcher.getListTweets(_list)
+        })
