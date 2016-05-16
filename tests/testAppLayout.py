feaa4d68b39767c28fa37f7a89886999543fbea8
Vasja Volin <vasja.volin@gmail.com>
2016-05-15 20:59:08 -0400

    add individual test for the different views we have

--- tests/testAppLayout.py
+++ tests/testAppLayout.py
@@ -8,15 +8,14 @@ class TestAppLayout(unittest.TestCase):
         self.term = Mock()
         self.al = AppLayout(screen=self.screen)
 
-        self.al.splashView = Mock()
-        self.al.mainView = Mock()
-
     def test_it_should_default_to_the_main_view(self):
         state = {
             'screen_width': 40,
             'screen_height': 20,
             'view': 'random',
         }
+        self.al.splashView = Mock()
+        self.al.mainView   = Mock()
         self.al.render(self.term, state)
 
         assert self.al.mainView.call_count   == 1
@@ -28,8 +27,29 @@ class TestAppLayout(unittest.TestCase):
             'screen_height': 20,
             'view': 'splash',
         }
+        self.al.splashView = Mock()
+        self.al.mainView   = Mock()
         self.al.render(self.term, state)
 
         assert self.al.mainView.call_count   == 0
         assert self.al.splashView.call_count == 1
 
+    def test_it_should_render_the_splash_view(self):
+        state = {
+            'screen_width': 40,
+            'screen_height': 20,
+            'view': 'splash',
+        }
+        self.assertEqual(len(self.al.splashView(state)), 1)
+
+    def test_it_should_render_the_main_view(self):
+        state = {
+            'screen_width': 40,
+            'screen_height': 20,
+            'selected_list': '',
+            'lists': {},
+            'cursor': 0,
+            'cursor_max': 20,
+            'view': 'splash',
+        }
+        self.assertEqual(len(self.al.mainView(state)), 3)
