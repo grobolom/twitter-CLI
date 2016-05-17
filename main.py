f0deb3c78355da806019ead518beea42beb78f51
Vasja Volin <vasja.volin@gmail.com>
2016-05-16 21:14:26 -0400

    fix incorrect config

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from queue import Queue
 
 import json
 from TweetSource import TweetFetcher
+from TweetSource import MongoTweetSource
 from TweetSource.utils import getTwitter
 from TweetSource import TwitterWrapper
 
 import pymongo
 
 def main():
-    mongo = pymongo.MongoClient('localhost', 27010)
+    mongo = pymongo.MongoClient('localhost', 27017)
     db = mongo['twitter-cli']
 
     with open('config/twitter.json') as twitter_config:
         config = json.load(twitter_config)
     twitter = getTwitter(config)
     tweetSource = TwitterWrapper(config, twitter)
-    tweetFetcher = TweetFetcher(tweetSource, db)
+    mongoSource = MongoTweetSource(db)
+    tweetFetcher = TweetFetcher(tweetSource, mongoSource)
 
     q = Queue()
     t = Thread(target=ts_func, args=(q, tweetFetcher))
