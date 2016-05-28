88738b05ca3240666aa83eaff41d8e68c000bd10
Vasja Volin <vasja.volin@gmail.com>
2016-05-28 16:07:34 -0400

    add store action dispatching

--- TwitterCLI/utils/Store.py
+++ TwitterCLI/utils/Store.py
@@ -1,3 +1,5 @@
+import copy
+
 class Store:
     def __init__(self, reducers, initialState):
         self.reducers = reducers
@@ -5,3 +7,9 @@ class Store:
 
     def getState(self):
         return self.state
+
+    def dispatch(self, action):
+        new_state = copy.deepcopy(self.state)
+        for reducer in self.reducers:
+            new_state = reducer.reduce(new_state, action)
+        self.state = new_state
--- tests/TwitterCLI/utils/testStore.py
+++ tests/TwitterCLI/utils/testStore.py
@@ -2,9 +2,18 @@ import unittest
 
 from TwitterCLI.utils import Store
 
+class MockReducer:
+    def reduce(self, state, action):
+        return []
+
 class TestStore(unittest.TestCase):
     def test_it_should_get_an_initial_state(self):
         initialState = { 'something': 'else' }
-        reducers = []
-        s = Store(reducers, initialState)
+        s = Store([], initialState)
         assert s.getState() == initialState
+
+    def test_it_should_dispatch_actions(self):
+        reducers = [ MockReducer() ]
+        s = Store(reducers, { 'something': 'else' })
+        s.dispatch({})
+        assert s.getState() == []
